ae036101202ee2378638e04db09fc1f4758a2a6b
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-12-05 16:20:08 +0100

    commit

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,11 @@
 <script setup>
+const config = useRuntimeConfig()
 let { slug } = useRoute().params
 
 const url = slug && slug.length > 0 ? slug.join('/') : 'home'
-const story = await useAsyncStoryblok(url, { version: 'draft' })
+const story = await useAsyncStoryblok(url, {
+  version: config.isPreview === 'true' ? 'draft' : 'published',
+})
 </script>
 
 <template>
