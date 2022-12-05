6ab7103e1b1548e62827d43d237ba647cb663976
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-12-05 11:19:28 +0100

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,14 @@ export default defineNuxtConfig({
       '@storyblok/nuxt',
       {
         accessToken: process.env.STORYBLOK_TOKEN,
-        bridge: true,
+        bridge: process.env.IS_PREVIEW === 'true' ? true : false,
       },
     ],
     '@nuxtjs/tailwindcss',
   ],
+  runtimeConfig: {
+    public: {
+      isPreview: process.env.IS_PREVIEW
+    }
+  }
 })
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,12 @@
 <script setup>
+const config = useRuntimeConfig()
 let { slug } = useRoute().params
 
 const url = slug && slug.length > 0 ? slug.join('/') : 'home'
-const story = await useAsyncStoryblok(url, { version: 'draft' })
+const story = await useAsyncStoryblok(url, { version: config.isPreview === 'true' ? 'draft' : 'public' })
 </script>
 
 <template>
+  <pre>{{config.isPreview}}</pre>
   <StoryblokComponent v-if="story" :blok="story.content" />
 </template>
