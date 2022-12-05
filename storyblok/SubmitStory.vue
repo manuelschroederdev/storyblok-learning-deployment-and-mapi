12ac96fe5bc5bde938903421e807653f947bb8d3
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-12-05 12:08:06 +0100

    commit

---
+++ storyblok/SubmitStory.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    v-editable="blok"
+    class="text-center"
+  >
+    <h3 class="text-2xl text-[#1d243d] font-bold">
+      {{ blok.headline }}
+    </h3>
+  </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object });
+</script>
