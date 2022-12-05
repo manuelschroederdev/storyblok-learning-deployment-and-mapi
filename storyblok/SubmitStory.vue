1e6f0aada302b4e32508809c58b5b09811df581a
Manuel Schröder <manuel.schroeder@storyblok.com>
2022-12-05 12:18:00 +0100

    commit

--- storyblok/SubmitStory.vue
+++ storyblok/SubmitStory.vue
@@ -1,14 +1,31 @@
 <template>
-  <div
-    v-editable="blok"
-    class="text-center"
-  >
+  <div v-editable="blok" class="text-center">
     <h3 class="text-2xl text-[#1d243d] font-bold">
       {{ blok.headline }}
     </h3>
+    <form class="py-12 flex flex-col max-w-xl mx-auto gap-y-6">
+      <input
+        type="text"
+        name="story-title"
+        id="story-title"
+        class="rounded-lg border-2 border-[#50b0ae] focus:outline-none py-2 px-4"
+        placeholder="Story title"
+      />
+      <textarea
+        name="story-content"
+        id="story-content"
+        cols="30"
+        rows="10"
+        class="rounded-lg border-2 border-[#50b0ae] focus:outline-none py-2 px-4"
+        placeholder="Story content"
+      ></textarea>
+      <button type="submit" class="rounded-lg px-4 py-2 font-bold bg-[#f7f6fd]">
+        Send
+      </button>
+    </form>
   </div>
 </template>
 
 <script setup>
-defineProps({ blok: Object });
+defineProps({ blok: Object })
 </script>
